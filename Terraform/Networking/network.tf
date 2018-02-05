e213c9a73b8c45e75437318eeac6dda96509e798
Marco Antonio Ramirez Barrios <mramirez@ad.actualisapbolivia.com>
2018-02-05 15:23:38 -0400

    Second script add
    Second script add

--- Terraform/Networking/network.tf
+++ Terraform/Networking/network.tf
@@ -1,31 +1,22 @@
-# Configure the Azure Provider
-provider "azurerm" { }
-
-# Create a resource group
-resource "azurerm_resource_group" "network" {
-  name     = "production"
-  location = "West US"
-}
-
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "network" {
-  name                = "production-network"
+resource "azurerm_virtual_network" "B1CDemo" {
+  name                = "B1CDemo-Network"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.network.location}"
-  resource_group_name = "${azurerm_resource_group.network.name}"
+  location            = "${azurerm_resource_group.B1CDemo.location}"
+  resource_group_name = "${azurerm_resource_group.B1CDemo.name}"
 
   subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
+    name           = "B1CDemo.subnet1"
+    address_prefix = "10.0.1.0/26"
   }
 
   subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
+    name           = "B1CDemo.subnet2"
+    address_prefix = "10.0.2.0/26"
   }
 
   subnet {
-    name           = "subnet3"
-    address_prefix = "10.0.3.0/24"
+    name           = "B1CDemo.subnet3"
+    address_prefix = "10.0.3.0/26"
   }
 }
